61a517f77ab0453c0240834bfcb3334d0ec84841
Mohit Maroliya <mmaroliya@gmail.com>
2022-03-07 03:13:15 +0530

    Adding the skills in About Page

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,32 @@ import React from "react";
 import Header from "./components/Header";
 import Image from "next/image";
 import imgurl from "../public/dog.jpg";
+import {
+  SiBootstrap,
+  SiExpress,
+  SiJavascript,
+  SiMongodb,
+  SiNextdotjs,
+  SiReact,
+  SiTailwindcss,
+  SiFramer,
+  SiMaterialui,
+  SiCplusplus,
+  SiPython,
+  SiMysql,
+  SiGit,
+  SiPostman,
+  SiDocker,
+  SiYoutubestudio,
+  SiHeroku,
+  SiNetlify,
+  SiVercel,
+  SiAdobephotoshop,
+  SiPhotopea,
+  SiLinux,
+  SiOverleaf,
+} from "react-icons/si";
+import { FaCss3, FaHtml5, FaNodeJs } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -31,11 +57,240 @@ const About = () => {
               Technology Jodhpur (2017-21) in CSE. Currently, I'm workings as a
               Software Development Engineer in a Software Company.
             </p>
-            
           </div>
-          <div className="w-1/5 bg-gray-100 h-[1px] mx-auto mb-5 hover:drop-shadow-2xl"></div> 
-          
+          <div className="w-1/5 bg-gray-100 h-[1px] mx-auto mb-5 hover:drop-shadow-2xl"></div>
+          <div className="max-w-7xl mx-auto md:py-5 lg:py-20 px-4 sm:px-2 lg:px-2">
+            <h2 className="text-4xl md:text-7xl xl:text-7xl leading-snug md:leading-snug xl:leading-relaxed w-full text-transparent text-center bg-clip-text font-extrabold bg-gradient-to-tl from-white to-sky-500/10 p-2">
+              My Skills
+            </h2>
+          </div>
+          <div className="text-center m-2">
+            <div>
+              <h1 class="text-4xl md:text-5xl xl:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 mb-4 lg:mb-6 xl:mb-8">
+                Web Development
+              </h1>
+            </div>
+            <div className="grid grid-cols-4 gap-5 p-5 px-3 sm:px-4 lg:px-10 lg:w-4/5 mx-auto ">
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiReact className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  ReactJS
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <FaNodeJs className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  NodeJS
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiExpress className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  ReactJS
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiNextdotjs className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  NextJS
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiTailwindcss className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  TailwindCSS
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <FaHtml5 className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  HTML-5
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <FaCss3 className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  CSS-3
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiJavascript className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  javaScript
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiBootstrap className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  BootStrap5
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiFramer className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Framer Motion
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiMaterialui className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  MaterialUI
+                </span>
+              </div>
+            </div>
+          </div>
+
+          {/*programming */}
+          <div className="text-center m-2">
+            <div>
+              <h1 class="text-4xl md:text-5xl xl:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 mb-4 lg:mb-6 xl:mb-8">
+                Programming
+              </h1>
+            </div>
+            <div className="grid grid-cols-4 gap-5 p-5 px-3 sm:px-4 lg:px-10 lg:w-4/5 mx-auto ">
+              <div className=" mx-auto flex-row justify-center items-center">
+                {/* empty  */}
+              </div>
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiCplusplus className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  C++
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiPython className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Python3
+                </span>
+              </div>
+            </div>
+          </div>
+          {/* databases */}
+          <div className="text-center m-2">
+            <div>
+              <h1 class="text-4xl md:text-5xl xl:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 mb-4 lg:mb-6 xl:mb-8">
+                Databases
+              </h1>
+            </div>
+            <div className="grid grid-cols-4 gap-5 p-5 px-3 sm:px-4 lg:px-10 lg:w-4/5 mx-auto ">
+              <div className=" mx-auto flex-row justify-center items-center">
+                {/* empty  */}
+              </div>
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiMysql className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  MySQL
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiMongodb className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  MongoDB
+                </span>
+              </div>
+            </div>
+          </div>
+          {/* utilities */}
+          <div className="text-center m-2">
+            <div>
+              <h1 class="text-4xl md:text-5xl xl:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 mb-4 lg:mb-6 xl:mb-8">
+                Utilities
+              </h1>
+            </div>
+            <div className="grid grid-cols-4 gap-5 p-5 px-3 sm:px-4 lg:px-10 lg:w-4/5 mx-auto ">
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiGit className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Git/Github
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiPostman className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  PostMan
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiDocker className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Docker
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiYoutubestudio className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  YTStudio
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiHeroku className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Heroku
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiNetlify className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Netlify
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiVercel className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Vercel
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiAdobephotoshop className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Adobe PS
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiPhotopea className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  PhotoPea
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiLinux className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Linux
+                </span>
+              </div>
+
+              <div className=" mx-auto flex-row justify-center items-center">
+                <SiOverleaf className="h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 xl:h-32 xl:w-32 hover:drop-shadow-3xl opacity-90 hover:opacity-100" />
+                <span className="text-center lg:text-lg sm:ml-1 md:ml-2 lg:ml-4 xl:ml-7 ">
+                  Overleaf
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <div className="w-1/5 bg-gray-100 h-[2px] mx-auto my-10 hover:drop-shadow-2xl"></div>
       </div>
     </>
   );
